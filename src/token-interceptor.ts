7a9a5a9b0cdb00ec2a81cfa92e2cf7bb74b44071
Federico Vitale <fedevitale99@gmail.com>
2022-11-22 13:54:01 +0100

    feat: debug options

--- src/token-interceptor.ts
+++ src/token-interceptor.ts
@@ -16,12 +16,14 @@ export interface ITokenInterceptorConfig {
   storage: Storage;
   requestRefresh: RequestRefresh;
   options?: ITokenOptions;
+  debug?: boolean;
 }
 
 export default class TokenInterceptor {
   private is_refreshing = false;
   queue: Queue<Token | undefined> = new Queue();
 
+  private debug = false;
   private storage: Storage;
   private requestRefresh: RequestRefresh;
   private config: Required<ITokenOptions> = {
@@ -35,6 +37,12 @@ export default class TokenInterceptor {
     this.storage = config.storage;
     this.requestRefresh = config.requestRefresh;
     this.config = { ...this.config, ...config.options };
+    this.debug = config.debug ?? false;
+  }
+
+  log(message?: any, ...args: any[]): void {
+    if (!this.debug) return;
+    console.debug(`[@aquacloud/axios-jwt]`, message, ...args);
   }
 
   get isRefreshing(): boolean {
@@ -120,6 +128,9 @@ export default class TokenInterceptor {
     if (!this.refreshToken) return requestConfig;
 
     if (this.is_refreshing) {
+      this.log(
+        `token is refreshing, enqueue the request: [${requestConfig.method}] ${requestConfig.url}`
+      );
       return new Promise<string | undefined>((resolve, reject) =>
         this.queue.enqueue({ resolve, reject })
       )
@@ -159,12 +170,14 @@ export default class TokenInterceptor {
   };
 
   async refreshTokenIfNeeded(): Promise<Token | undefined> {
-    let accessToken = this.accessToken;
+    const accessToken = this.accessToken;
     const leeway = ms(this.config.leeway as string);
 
     if (!accessToken || isExpired(accessToken, leeway)) {
-      accessToken = await this.doRefresh();
+      this.log("Invalid access token or expired token. Refreshing...");
+      const newToken = await this.doRefresh();
       this.accessToken = accessToken;
+      return newToken;
     }
 
     return accessToken;
@@ -172,11 +185,13 @@ export default class TokenInterceptor {
 
   private async doRefresh() {
     if (!this.tokens || !this.refreshToken) {
+      this.log("unable to refresh. no refresh token available");
       throw new Error("Unable to refresh tokens. No refresh token available.");
     }
 
     try {
       this.is_refreshing = true;
+      this.log("refresh started.");
       const newTokens = await this.requestRefresh(this.tokens);
 
       if (newTokens === null) {
@@ -186,18 +201,24 @@ export default class TokenInterceptor {
 
       if (typeof newTokens === "object" && newTokens.accessToken) {
         this.tokens = newTokens;
+        this.log("refresh completed");
         return newTokens.accessToken;
       } else if (typeof newTokens === "string") {
         this.accessToken = newTokens;
+        this.log("refresh completed");
         return newTokens;
       }
 
+      this.is_refreshing = false;
+      this.log("refresh failed");
       throw new Error(
         `"requestRefresh" must either return a string or an object with an "accessToken"`
       );
     } catch (error) {
+      this.is_refreshing = false;
       if (axios.isAxiosError(error)) {
         const status = error.response?.status;
+        this.log("refresh error with status code: ", status);
 
         if (status === 401 || status === 422) {
           this.storage.removeItem(this.config.storageKey);
@@ -208,11 +229,13 @@ export default class TokenInterceptor {
       }
 
       if (error instanceof Error) {
+        this.log("refresh failed: ", error.message);
         throw new Error(`Failed to refresh auth token: ${error.message}`);
       }
 
-      throw new Error(`Failed to refrehs auth token and failed to parse error`);
+      throw new Error(`Failed to refresh auth token and failed to parse error`);
     } finally {
+      this.log("refresh completed");
       this.is_refreshing = false;
     }
   }
